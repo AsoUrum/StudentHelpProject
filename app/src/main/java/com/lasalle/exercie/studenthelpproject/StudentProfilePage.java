5d802954ca7d6d30c16f579821fc414b036cc038
ChenXiao <chen713x@gmail.com>
2021-11-28 21:13:57 -0500

    Finish the intent from main page to student login page, and student login to profile and logout

--- app/src/main/java/com/lasalle/exercie/studenthelpproject/StudentProfilePage.java
+++ app/src/main/java/com/lasalle/exercie/studenthelpproject/StudentProfilePage.java
@@ -2,13 +2,40 @@ package com.lasalle.exercie.studenthelpproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class StudentProfilePage extends AppCompatActivity {
+public class StudentProfilePage extends AppCompatActivity implements View.OnClickListener  {
 
+    Button btnStudentLogout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_student_profile_page);
+        initialize();
+    }
+
+    private void initialize() {
+        btnStudentLogout = findViewById(R.id.btnStudentLogout);
+        btnStudentLogout.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        int id = view.getId();
+
+        switch (id){
+
+            case R.id.btnStudentLogout:
+                toStudentLoginPage();
+                break;
     }
 }
+
+    private void toStudentLoginPage() {
+        Intent i = new Intent(this ,StudentLoginPage.class);
+        startActivity(i);
+    }
+    }
