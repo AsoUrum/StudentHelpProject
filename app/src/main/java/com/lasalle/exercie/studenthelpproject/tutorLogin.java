1d06f2640f953b12c551ad3983ecd9f75bb8a3a2
Abhishek <abhishekdhingra1592@gmail.com>
2021-11-29 16:08:36 -0500

    Adding intent

--- app/src/main/java/com/lasalle/exercie/studenthelpproject/tutorLogin.java
+++ app/src/main/java/com/lasalle/exercie/studenthelpproject/tutorLogin.java
@@ -2,13 +2,41 @@ package com.lasalle.exercie.studenthelpproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class tutorLogin extends AppCompatActivity {
+public class tutorLogin extends AppCompatActivity implements View.OnClickListener {
+
+    Button btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tutor_login);
+        initialize();
+    }
+
+    private void initialize() {
+        btnLogin = findViewById(R.id.btnLogin);
+    }
+
+    private void showProfilePage()
+    {
+        Intent intent = new Intent(this, tutorProfile.class );
+        startActivity(intent);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        int id = view.getId();
+        switch(id){
+            case R.id.btnLogin:
+                showProfilePage();
+                break;
+        }
+
     }
 }
--- app/src/main/java/com/lasalle/exercie/studenthelpproject/tutorProfile.java
+++ app/src/main/java/com/lasalle/exercie/studenthelpproject/tutorProfile.java
@@ -2,13 +2,42 @@ package com.lasalle.exercie.studenthelpproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class tutorProfile extends AppCompatActivity {
+public class tutorProfile extends AppCompatActivity implements View.OnClickListener {
+
+    Button btnLogout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tutor_profile);
+
+        initialize();
+    }
+
+    private void initialize() {
+        btnLogout = findViewById(R.id.btnLogout);
+
+    }
+
+    private void showLoginPage()
+    {
+        Intent intent = new Intent(this, tutorLogin.class );
+        startActivity(intent);
+    }
+
+    @Override
+    public void onClick(View view) {
+        int id = view.getId();
+        switch(id){
+            case R.id.btnLogin:
+                showLoginPage();
+                break;
+        }
+
     }
 }
